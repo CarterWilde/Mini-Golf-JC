a00a90735a8d5e213c974cace324d75850ca5997
CarterWilde <carterjwilde@gmail.com>
2018-05-10 14:26:58 -0600

    renderer init

---
+++ src/Renderer.java
@@ -0,0 +1,9 @@
+
+public class Renderer extends JComponent{
+	public Queue<JComponent> renderQueue;
+	public Renderer(){
+		renderQueue = new Queue<JComponent>();
+	}
+	public void liveItem(){
+	}
+}
