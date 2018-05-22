929d5f1a5b1446a8667961c04503648cbb984290
JustinCSnider <34716875+JustinCSnider@users.noreply.github.com>
2018-05-22 13:38:50 -0600

    Small effective changes

--- MiniGolf/Controllers/MiniGolf.java
+++ MiniGolf/Controllers/MiniGolf.java
@@ -4,5 +4,7 @@ import Views.*;
 public class MiniGolf extends Game{
     public MiniGolf(boolean sb){
         super(sb);
+        Rectangle r = new Rectangle(300,100,25,100);
+        ui.addGameObject(r);
     }
 }
